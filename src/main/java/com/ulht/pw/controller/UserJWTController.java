323982320c6ed1a8eb4d8dfe69366fbbfd55ef47
Pedro Perdigão <admin@example.com>
2019-04-30 09:20:19 +0100

    added account service

--- src/main/java/com/ulht/pw/controller/UserJWTController.java
+++ src/main/java/com/ulht/pw/controller/UserJWTController.java
@@ -9,15 +9,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ulht.pw.controller.rest.errors.InternalServerErrorException;
 import com.ulht.pw.controller.vm.LoginVM;
+import com.ulht.pw.dto.user.UserDTO;
 import com.ulht.pw.security.jwt.JWTFilter;
 import com.ulht.pw.security.jwt.TokenProvider;
+import com.ulht.pw.service.UserService;
 
 /**
  * Controller to authenticate users.
@@ -27,11 +31,14 @@ import com.ulht.pw.security.jwt.TokenProvider;
 public class UserJWTController {
 	private final TokenProvider tokenProvider;
 
+	private final UserService userService;
+
 	private final AuthenticationManager authenticationManager;
 
-	public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+	public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager, UserService userService) {
 		this.tokenProvider = tokenProvider;
 		this.authenticationManager = authenticationManager;
+		this.userService = userService;
 	}
 
 	@PostMapping("/authenticate")
@@ -47,6 +54,19 @@ public class UserJWTController {
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
 		return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+	}
+
+	/**
+	 * GET /account : get the current user.
+	 *
+	 * @return the current user
+	 * @throws RuntimeException
+	 *             500 (Internal Server Error) if the user couldn't be returned
+	 */
+	@GetMapping("/account")
+	public UserDTO getAccount() {
+		return userService.getUserWithAuthorities().map(UserDTO::new)
+				.orElseThrow(() -> new InternalServerErrorException("User could not be found"));
 	}
 
 	/**
